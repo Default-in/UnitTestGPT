9b09be4858523620b0836ce31f2b0efc0717ecf0
Yogeesh H K <yogeesh@getdefault.in>
2023-09-29 08:10:17 +0530

    updates celery configuration

--- settings/base.py
+++ settings/base.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 import socket
 from decouple import Config, RepositoryEnv
@@ -133,6 +133,7 @@ CELERY_CREATE_MISSING_QUEUES = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATIC_URL = "static/"
 
